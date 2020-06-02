20abc8fe9c0b70421e276ecb51429936b41dd60d
donovanherion <donovanherion@gmail.com>
2020-06-02 14:43:55 +0200

    fixed bug

---
+++ assets/app.js
@@ -0,0 +1,18 @@
+console.log('hello');
+console.log('again');
+const open = document.querySelector('.open-hamburger');
+const close = document.querySelector('.close-hamburger')
+const menu = document.querySelector('.nav-hamburger');
+
+open.addEventListener("click", function() {
+    menu.style.opacity = 0.95;
+    menu.style.zIndex = 2;
+})
+
+close.addEventListener("click", function() {
+    menu.style.opacity = 0;
+    menu.style.zIndex = -2;
+})
+console.log(open);
+console.log(menu);
+    
