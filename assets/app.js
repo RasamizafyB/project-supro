00166be0f5dfa57778a1e2b936592aca65cf3cae
donovanherion <donovanherion@gmail.com>
2020-06-04 10:29:49 +0200

    header and footer replaced

--- assets/app.js
+++ assets/app.js
@@ -1,18 +1,3 @@
-//Hamburger Menu
-const open = document.querySelector('.open-hamburger');
-const close = document.querySelector('.close-hamburger')
-const menu = document.querySelector('.nav-hamburger');
-
-open.addEventListener("click", function() {
-    menu.style.opacity = 0.95;
-    menu.style.zIndex = 2;
-})
-
-close.addEventListener("click", function() {
-    menu.style.opacity = 0;
-    menu.style.zIndex = -2;
-})
-
 //Time for the deal of the day
 
 const hour = document.querySelector('.hours');
